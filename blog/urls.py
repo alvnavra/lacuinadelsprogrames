efc55cd160b46bb211041cd4c3b3676a22dc7359
alvnavra <alvnavra@gmail.com>
2019-08-14 12:14:23 +0200

    Inicio del Proceso de Locale (settings, again)

--- blog/urls.py
+++ blog/urls.py
@@ -26,8 +26,6 @@ urlpatterns = i18n_patterns(
 
 )
 
-print (urlpatterns)
-
 if settings.DEBUG:
     urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
     urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
